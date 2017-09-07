9e230c7baf6f162165398a1d1bc3121029066452
DimaFreund <dimafreund@ukr.net>
2017-09-07 18:20:14 +0300

    work 07

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,32 @@
 $(function() {
 
 	$('#top-background-carousel').owlCarousel({
-    loop:true,
-    nav:true,
-    items: 1
+    loop: true,
+    items: 1,
+    nav: true,
+  	navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+   	autoplay: true,
 })
+
+
+	$('#teams-carousel').owlCarousel({
+    loop: true,
+    items: 4,
+    nav: true,
+  	navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+   	autoplay: true,
+})
+
+
+$(".main").onepage_scroll({
+   sectionContainer: "section", // контейнер, к которому будет применяться скролл
+   easing: "ease", // Тип анимации "ease", "linear", "ease-in", "ease-out", "ease-in-out"
+   animationTime: 1000, // время анимации
+   pagination: true, // скрыть или отобразить пагинатор
+   updateURL: false // обновлять URL или нет
+
+});
+
+
 
 });
